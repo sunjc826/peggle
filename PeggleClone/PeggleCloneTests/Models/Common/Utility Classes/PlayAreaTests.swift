60e502142a6511c0ff4c80ef71a3a2536511e987
Sun Jia Cheng <sunjc826@gmail.com>
2022-02-27 19:38:15 +0800

    Fix tests

--- PeggleClone/PeggleCloneTests/Models/Common/Utility Classes/PlayAreaTests.swift
+++ PeggleClone/PeggleCloneTests/Models/Common/Utility Classes/PlayAreaTests.swift
@@ -10,7 +10,7 @@ class PlayAreaTests: XCTestCase {
     var playArea: PlayArea!
     override func setUpWithError() throws {
         try super.setUpWithError()
-        playArea = PlayArea(width: 10, height: 10, cannonZoneHeight: 1)
+        playArea = PlayArea(width: 10, height: 10, cannonZoneHeight: 1, bucketZoneHeight: 1)
     }
 
     override func tearDownWithError() throws {
@@ -19,16 +19,16 @@ class PlayAreaTests: XCTestCase {
     }
 
     func testEqual_differentPlayAreaSize_returnsFalse() {
-        let anyDifferentPlayArea1 = PlayArea(width: 10, height: 9, cannonZoneHeight: 1)
+        let anyDifferentPlayArea1 = PlayArea(width: 10, height: 9, cannonZoneHeight: 1, bucketZoneHeight: 1)
 
-        let anyDifferentPlayArea2 = PlayArea(width: 9, height: 10, cannonZoneHeight: 1)
+        let anyDifferentPlayArea2 = PlayArea(width: 9, height: 10, cannonZoneHeight: 1, bucketZoneHeight: 1)
 
         XCTAssertNotEqual(playArea, anyDifferentPlayArea1)
         XCTAssertNotEqual(playArea, anyDifferentPlayArea2)
     }
 
     func testEqual_samePlayAreaSize_returnsTrue() {
-        let anySamePlayArea = PlayArea(width: 10, height: 10, cannonZoneHeight: 1)
+        let anySamePlayArea = PlayArea(width: 10, height: 10, cannonZoneHeight: 1, bucketZoneHeight: 1)
 
         XCTAssertEqual(playArea, anySamePlayArea)
     }
